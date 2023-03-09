87b8fe45a94bb18a230fedb33d13f550c5fa301f
Gerald Mnyaki <gmnyaki@gmail.com>
2023-03-09 06:28:43 -0700

    Added comments

--- app/Http/Controllers/Posts/PostController.php
+++ app/Http/Controllers/Posts/PostController.php
@@ -10,31 +10,44 @@ use App\Models\Post;
 
 class PostController extends Controller
 {
-	public function __construct()
-	{
-		$this->middleware('auth')->only('store');
-	}
-	//show blade view
+    public function __construct()
+    {
+        $this->middleware('auth')->only('store');
+    }
+    //show blade view
     public function index()
     {
-    	$posts = Post::with('user')->latest()->paginate(5);//optimized for earger loading
+        $posts = Post::with('user')->latest()->paginate(3);//optimized for earger loading
 
-    	return view('posts.index', compact('posts'));
+        return view('posts.index', compact('posts'));
     }
-    //Store post in the database
+    /**
+     * Store a newly created post in storage.
+     *
+     * @param  \App\Http\Requests\Post\StorePostRequest  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function store(StorePostRequest $request)
     {
-    	$request->user()->posts()->create($request->only('body'));
+        $request->user()->posts()->create($request->only('body'));
 
-    	return back();
+        return back();
     }
-    //Delete post 
+    /**
+     * Destroy the specified resource from database.
+     *
+     * @param  Illuminate\Http\Request $request
+     * @param  \App\Models\Post  $post
+     * @return mixed
+     */ 
     public function destroy(Request $request, Post $post)
     {
-    	$this->authorize('delete', $post);
-    	$post->delete();
+        // Determine if the user is authorized to delete the post.
+        $this->authorize('delete', $post);
 
-    	return back();
-    	 
+        $post->delete();
+
+        return back();
+         
     }
 }
--- app/Http/Controllers/Users/UserPostController.php
+++ app/Http/Controllers/Users/UserPostController.php
@@ -1,5 +1,6 @@
 <?php
 
+// Define the namespace and import required classes.
 namespace App\Http\Controllers\Users;
 
 use App\Http\Controllers\Controller;
@@ -8,9 +9,10 @@ use App\Models\User;
 
 class UserPostController extends Controller
 {
+	// Define a method that retrieves a user's posts and returns them in a paginated view.
     public function index(User $user)
     {
-    	$posts = $user->posts()->paginate(5);
+    	$posts = $user->posts()->paginate(5);// Retrieve the user's posts and paginate them.
     	return view('users.posts.index', compact('posts', 'user'));
     		
     }
